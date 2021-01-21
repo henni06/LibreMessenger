SuwiEtch/briar

Implement UI for setting profile pictures

briar-android/src/main/java/org/briarproject/briar/android/settings/ConfirmAvatarDialogFragment.java
@@ -1,0 +1,94 @@
+package org.briarproject.briar.android.settings;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.briarproject.briar.R;
+import org.briarproject.briar.android.activity.BaseActivity;
+
+import javax.inject.Inject;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import static java.util.Objects.requireNonNull;
+
+public class ConfirmAvatarDialogFragment extends DialogFragment {
+
+	final static String TAG = ConfirmAvatarDialogFragment.class.getName();
+
+	@Inject
+	ViewModelProvider.Factory viewModelFactory;
+	private SettingsViewModel settingsViewModel;
+
+	private static final String ARG_URI = "uri";
+	private Uri uri;
+
+	static ConfirmAvatarDialogFragment newInstance(Uri uri) {
+		ConfirmAvatarDialogFragment f = new ConfirmAvatarDialogFragment();
+
+		Bundle args = new Bundle();
+		args.putString(ARG_URI, uri.toString());
+		f.setArguments(args);
+
+		return f;
+	}
+
+	@Override
+	public void onAttach(Context ctx) {
+		super.onAttach(ctx);
+		((BaseActivity) requireActivity()).getActivityComponent().inject(this);
+	}
+
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		Bundle args = requireArguments();
+		String argUri = requireNonNull(args.getString(ARG_URI));
+		uri = Uri.parse(argUri);
+
+		FragmentActivity activity = requireActivity();
+
+		ViewModelProvider provider =
+				new ViewModelProvider(activity, viewModelFactory);
+		settingsViewModel = provider.get(SettingsViewModel.class);
+		settingsViewModel.onCreate();
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+
+		LayoutInflater factory = LayoutInflater.from(getContext());
+		final View view =
+				factory.inflate(R.layout.fragment_confirm_avatar_dialog, null);
+		builder.setView(view);
+
+		builder.setTitle(R.string.dialog_confirm_profile_picture_title)
+				.setMessage(R.string.dialog_confirm_profile_picture_question);
+		builder.setNegativeButton(R.string.cancel, null);
+		builder.setPositiveButton(R.string.dialog_confirm_profile_picture_set,
+				(dialog, id) -> {
+					trySetAvatar();
+				});
+
+		ImageView imageView = view.findViewById(R.id.image);
+		imageView.setImageResource(R.drawable.contact_connected);
+		imageView.setImageURI(uri);
+
+		settingsViewModel.getOurAuthorInfo().observe(activity, us -> {
+			TextView textViewUserName = view.findViewById(R.id.username);
+			textViewUserName.setText(us.getLocalAuthor().getName());
+		});
+
+		return builder.create();
+	}
+
+	private void trySetAvatar() {
+		settingsViewModel.setAvatar(uri);
+	}
+
+}

briar-android/src/main/java/org/briarproject/briar/android/settings/SettingsActivity.java
@@ -1,15 +1,41 @@
 package org.briarproject.briar.android.settings;
 
+import android.content.ClipData;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import org.briarproject.briar.R;
 import org.briarproject.briar.android.activity.ActivityComponent;
 import org.briarproject.briar.android.activity.BriarActivity;
+import org.briarproject.briar.android.view.AuthorView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
+import androidx.lifecycle.ViewModelProvider;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.content.Intent.ACTION_GET_CONTENT;
+import static android.content.Intent.ACTION_OPEN_DOCUMENT;
+import static android.content.Intent.CATEGORY_OPENABLE;
+import static android.content.Intent.EXTRA_MIME_TYPES;
+import static android.os.Build.VERSION.SDK_INT;
+import static org.briarproject.bramble.util.AndroidUtils.getSupportedImageContentTypes;
+import static org.briarproject.briar.android.activity.RequestCodes.REQUEST_AVATAR_IMAGE;
 
 public class SettingsActivity extends BriarActivity {
+
+	@Inject
+	ViewModelProvider.Factory viewModelFactory;
+	private SettingsViewModel settingsViewModel;
 
 	@Override
 	public void onCreate(Bundle bundle) {
@@ -22,6 +48,25 @@ public class SettingsActivity extends BriarActivity {
 		}
 
 		setContentView(R.layout.activity_settings);
+
+		ViewModelProvider provider =
+				new ViewModelProvider(this, viewModelFactory);
+		settingsViewModel = provider.get(SettingsViewModel.class);
+		settingsViewModel.onCreate();
+
+		settingsViewModel.getOurAuthorInfo().observe(this, us -> {
+			TextView textViewUserName = findViewById(R.id.avatarTitle);
+			textViewUserName.setText(us.getLocalAuthor().getName());
+
+			CircleImageView imageViewAvatar = findViewById(R.id.avatarImage);
+			AuthorView.setAvatar(imageViewAvatar, us.getLocalAuthor().getId(),
+					us.getAuthorInfo());
+		});
+
+		View avatarGroup = findViewById(R.id.avatarGroup);
+		avatarGroup.setOnClickListener(e -> {
+			selectAvatarImage();
+		});
 	}
 
 	@Override
@@ -37,4 +82,58 @@ public class SettingsActivity extends BriarActivity {
 		}
 		return false;
 	}
+
+	private void selectAvatarImage() {
+		Intent intent = getAvatarFileIntent();
+		startActivityForResult(intent, REQUEST_AVATAR_IMAGE);
+	}
+
+	private Intent getAvatarFileIntent() {
+		// TODO: I copied this from TextAttachmentController#getAttachFileIntent,
+		// maybe we should centralize it into a utility class.
+		Intent intent = new Intent(SDK_INT >= 19 ?
+				ACTION_OPEN_DOCUMENT : ACTION_GET_CONTENT);
+		intent.setType("image/*");
+		intent.addCategory(CATEGORY_OPENABLE);
+		if (SDK_INT >= 19)
+			intent.putExtra(EXTRA_MIME_TYPES, getSupportedImageContentTypes());
+		return intent;
+	}
+
+	@Override
+	protected void onActivityResult(int request, int result,
+			@Nullable Intent data) {
+		super.onActivityResult(request, result, data);
+
+		if (request == REQUEST_AVATAR_IMAGE && result == RESULT_OK) {
+			onAvatarImageReceived(data);
+		}
+	}
+
+	private void onAvatarImageReceived(Intent resultData) {
+		if (resultData == null) return;
+		List<Uri> uris = new ArrayList<>();
+		if (resultData.getData() != null) {
+			uris.add(resultData.getData());
+		} else if (SDK_INT >= 18 && resultData.getClipData() != null) {
+			ClipData clipData = resultData.getClipData();
+			for (int i = 0; i < clipData.getItemCount(); i++) {
+				uris.add(clipData.getItemAt(i).getUri());
+			}
+		}
+		if (uris.isEmpty()) {
+			// TODO: show message that an image needs to be selected
+			return;
+		} else if (uris.size() != 1) {
+			// TODO: show message that only one image should be selected
+			return;
+		}
+		Uri uri = uris.get(0);
+
+		ConfirmAvatarDialogFragment dialog =
+				ConfirmAvatarDialogFragment.newInstance(uri);
+		dialog.show(getSupportFragmentManager(),
+				ConfirmAvatarDialogFragment.TAG);
+	}
+
 }

henni06/LibreMessenger

briar-android/src/main/java/org/briarproject/briar/android/settings/ConfirmAvatarDialogFragment.java
@@ -1,0 +1,90 @@
+package org.briarproject.briar.android.settings;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.briarproject.briar.R;
+import org.briarproject.briar.android.activity.BaseActivity;
+
+import javax.inject.Inject;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import static java.util.Objects.requireNonNull;
+
+public class ConfirmAvatarDialogFragment extends DialogFragment {
+
+	final static String TAG = ConfirmAvatarDialogFragment.class.getName();
+
+	@Inject
+	ViewModelProvider.Factory viewModelFactory;
+	private SettingsViewModel settingsViewModel;
+
+	private static final String ARG_URI = "uri";
+	private Uri uri;
+
+	static ConfirmAvatarDialogFragment newInstance(Uri uri) {
+		ConfirmAvatarDialogFragment f = new ConfirmAvatarDialogFragment();
+
+		Bundle args = new Bundle();
+		args.putString(ARG_URI, uri.toString());
+		f.setArguments(args);
+
+		return f;
+	}
+
+	@Override
+	public void onAttach(Context ctx) {
+		super.onAttach(ctx);
+		((BaseActivity) requireActivity()).getActivityComponent().inject(this);
+	}
+
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		Bundle args = requireArguments();
+		String argUri = requireNonNull(args.getString(ARG_URI));
+		uri = Uri.parse(argUri);
+
+		FragmentActivity activity = requireActivity();
+
+		ViewModelProvider provider =
+				new ViewModelProvider(activity, viewModelFactory);
+		settingsViewModel = provider.get(SettingsViewModel.class);
+		settingsViewModel.onCreate();
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+
+		LayoutInflater factory = LayoutInflater.from(getContext());
+		final View view =
+				factory.inflate(R.layout.fragment_confirm_avatar_dialog, null);
+		builder.setView(view);
+
+		builder.setTitle(R.string.dialog_confirm_profile_picture_title)
+				.setMessage(R.string.dialog_confirm_profile_picture_question);
+		builder.setNegativeButton(R.string.cancel, null);
+		builder.setPositiveButton(R.string.dialog_confirm_profile_picture_set,
+				(dialog, id) -> {
+					settingsViewModel.setAvatar(uri);
+				});
+
+		ImageView imageView = view.findViewById(R.id.image);
+		imageView.setImageResource(R.drawable.contact_connected);
+		imageView.setImageURI(uri);
+
+		settingsViewModel.getOwnIdentityInfo().observe(activity, us -> {
+			TextView textViewUserName = view.findViewById(R.id.username);
+			textViewUserName.setText(us.getLocalAuthor().getName());
+		});
+
+		return builder.create();
+	}
+
+}

briar-android/src/main/java/org/briarproject/briar/android/settings/SettingsActivity.java
@@ -1,15 +1,36 @@
 package org.briarproject.briar.android.settings;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
+import org.briarproject.bramble.api.FeatureFlags;
 import org.briarproject.briar.R;
 import org.briarproject.briar.android.activity.ActivityComponent;
 import org.briarproject.briar.android.activity.BriarActivity;
+import org.briarproject.briar.android.util.UiUtils;
+import org.briarproject.briar.android.view.AuthorView;
 
+import javax.inject.Inject;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
+import androidx.lifecycle.ViewModelProvider;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static org.briarproject.briar.android.activity.RequestCodes.REQUEST_AVATAR_IMAGE;
 
 public class SettingsActivity extends BriarActivity {
+
+	@Inject
+	ViewModelProvider.Factory viewModelFactory;
+	private SettingsViewModel settingsViewModel;
+
+	@Inject
+	FeatureFlags featureFlags;
 
 	@Override
 	public void onCreate(Bundle bundle) {
@@ -22,6 +43,32 @@ public class SettingsActivity extends BriarActivity {
 		}
 
 		setContentView(R.layout.activity_settings);
+
+		if (featureFlags.shouldEnableProfilePictures()) {
+			ViewModelProvider provider =
+					new ViewModelProvider(this, viewModelFactory);
+			settingsViewModel = provider.get(SettingsViewModel.class);
+			settingsViewModel.onCreate();
+
+			settingsViewModel.getOwnIdentityInfo().observe(this, us -> {
+				TextView textViewUserName = findViewById(R.id.username);
+				textViewUserName.setText(us.getLocalAuthor().getName());
+
+				CircleImageView imageViewAvatar =
+						findViewById(R.id.avatarImage);
+				AuthorView
+						.setAvatar(imageViewAvatar, us.getLocalAuthor().getId(),
+								us.getAuthorInfo());
+			});
+
+			View avatarGroup = findViewById(R.id.avatarGroup);
+			avatarGroup.setOnClickListener(e -> {
+				selectAvatarImage();
+			});
+		} else {
+			View view = findViewById(R.id.avatarGroup);
+			view.setVisibility(View.GONE);
+		}
 	}
 
 	@Override
@@ -37,4 +84,31 @@ public class SettingsActivity extends BriarActivity {
 		}
 		return false;
 	}
+
+	private void selectAvatarImage() {
+		Intent intent = UiUtils.createSelectImageIntent(false);
+		startActivityForResult(intent, REQUEST_AVATAR_IMAGE);
+	}
+
+	@Override
+	protected void onActivityResult(int request, int result,
+			@Nullable Intent data) {
+		super.onActivityResult(request, result, data);
+
+		if (request == REQUEST_AVATAR_IMAGE && result == RESULT_OK) {
+			onAvatarImageReceived(data);
+		}
+	}
+
+	private void onAvatarImageReceived(@Nullable Intent resultData) {
+		if (resultData == null) return;
+		Uri uri = resultData.getData();
+		if (uri == null) return;
+
+		ConfirmAvatarDialogFragment dialog =
+				ConfirmAvatarDialogFragment.newInstance(uri);
+		dialog.show(getSupportFragmentManager(),
+				ConfirmAvatarDialogFragment.TAG);
+	}
+
 }