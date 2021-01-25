PhantomPhone/briar

Perform thread list core access within a single transaction

briar-core/src/main/java/org/briarproject/briar/forum/ForumManagerImpl.java
@@ -189,6 +189,15 @@ class ForumManagerImpl extends BdfIncomingMessageHook implements ForumManager {
 		}
 	}
 
+	@Override
+	public String getPostText(Transaction txn, MessageId m) throws DbException {
+		try {
+			return getPostText(clientHelper.getMessageAsList(txn, m));
+		} catch (FormatException e) {
+			throw new DbException(e);
+		}
+	}
+
 	private String getPostText(BdfList body) throws FormatException {
 		// Parent ID, author, text, signature
 		return body.getString(2);
@@ -197,33 +206,35 @@ class ForumManagerImpl extends BdfIncomingMessageHook implements ForumManager {
 	@Override
 	public Collection<ForumPostHeader> getPostHeaders(GroupId g)
 			throws DbException {
+		return db.transactionWithResult(true, txn -> getPostHeaders(txn, g));
+	}
+
+	@Override
+	public List<ForumPostHeader> getPostHeaders(Transaction txn, GroupId g)
+			throws DbException {
 		try {
-			return db.transactionWithResult(true, txn -> {
-				Collection<ForumPostHeader> headers = new ArrayList<>();
-				Map<MessageId, BdfDictionary> metadata =
-						clientHelper.getMessageMetadataAsDictionary(txn, g);
-				// get all authors we need to get the info for
-				Set<AuthorId> authors = new HashSet<>();
-				for (Entry<MessageId, BdfDictionary> entry :
-						metadata.entrySet()) {
-					BdfList authorList = entry.getValue().getList(KEY_AUTHOR);
-					Author a = clientHelper.parseAndValidateAuthor(authorList);
-					authors.add(a.getId());
-				}
-				// get information for all authors
-				Map<AuthorId, AuthorInfo> authorInfos = new HashMap<>();
-				for (AuthorId id : authors) {
-					authorInfos.put(id, contactManager.getAuthorInfo(txn, id));
-				}
-				// Parse the metadata
-				for (Entry<MessageId, BdfDictionary> entry :
-						metadata.entrySet()) {
-					BdfDictionary meta = entry.getValue();
-					headers.add(getForumPostHeader(txn, entry.getKey(), meta,
-							authorInfos));
-				}
-				return headers;
-			});
+			List<ForumPostHeader> headers = new ArrayList<>();
+			Map<MessageId, BdfDictionary> metadata =
+					clientHelper.getMessageMetadataAsDictionary(txn, g);
+			// get all authors we need to get the info for
+			Set<AuthorId> authors = new HashSet<>();
+			for (Entry<MessageId, BdfDictionary> entry : metadata.entrySet()) {
+				BdfList authorList = entry.getValue().getList(KEY_AUTHOR);
+				Author a = clientHelper.parseAndValidateAuthor(authorList);
+				authors.add(a.getId());
+			}
+			// get information for all authors
+			Map<AuthorId, AuthorInfo> authorInfos = new HashMap<>();
+			for (AuthorId id : authors) {
+				authorInfos.put(id, contactManager.getAuthorInfo(txn, id));
+			}
+			// Parse the metadata
+			for (Entry<MessageId, BdfDictionary> entry : metadata.entrySet()) {
+				BdfDictionary meta = entry.getValue();
+				headers.add(getForumPostHeader(txn, entry.getKey(), meta,
+						authorInfos));
+			}
+			return headers;
 		} catch (FormatException e) {
 			throw new DbException(e);
 		}

henni06/LibreMessenger

briar-core/src/main/java/org/briarproject/briar/forum/ForumManagerImpl.java
@@ -192,6 +192,15 @@ class ForumManagerImpl extends BdfIncomingMessageHook implements ForumManager {
 		}
 	}
 
+	@Override
+	public String getPostText(Transaction txn, MessageId m) throws DbException {
+		try {
+			return getPostText(clientHelper.getMessageAsList(txn, m));
+		} catch (FormatException e) {
+			throw new DbException(e);
+		}
+	}
+
 	private String getPostText(BdfList body) throws FormatException {
 		// Parent ID, author, text, signature
 		return body.getString(2);
@@ -200,33 +209,35 @@ class ForumManagerImpl extends BdfIncomingMessageHook implements ForumManager {
 	@Override
 	public Collection<ForumPostHeader> getPostHeaders(GroupId g)
 			throws DbException {
+		return db.transactionWithResult(true, txn -> getPostHeaders(txn, g));
+	}
+
+	@Override
+	public List<ForumPostHeader> getPostHeaders(Transaction txn, GroupId g)
+			throws DbException {
 		try {
-			return db.transactionWithResult(true, txn -> {
-				Collection<ForumPostHeader> headers = new ArrayList<>();
-				Map<MessageId, BdfDictionary> metadata =
-						clientHelper.getMessageMetadataAsDictionary(txn, g);
-				// get all authors we need to get the info for
-				Set<AuthorId> authors = new HashSet<>();
-				for (Entry<MessageId, BdfDictionary> entry :
-						metadata.entrySet()) {
-					BdfList authorList = entry.getValue().getList(KEY_AUTHOR);
-					Author a = clientHelper.parseAndValidateAuthor(authorList);
-					authors.add(a.getId());
-				}
-				// get information for all authors
-				Map<AuthorId, AuthorInfo> authorInfos = new HashMap<>();
-				for (AuthorId id : authors) {
-					authorInfos.put(id, authorManager.getAuthorInfo(txn, id));
-				}
-				// Parse the metadata
-				for (Entry<MessageId, BdfDictionary> entry :
-						metadata.entrySet()) {
-					BdfDictionary meta = entry.getValue();
-					headers.add(getForumPostHeader(txn, entry.getKey(), meta,
-							authorInfos));
-				}
-				return headers;
-			});
+			List<ForumPostHeader> headers = new ArrayList<>();
+			Map<MessageId, BdfDictionary> metadata =
+					clientHelper.getMessageMetadataAsDictionary(txn, g);
+			// get all authors we need to get the info for
+			Set<AuthorId> authors = new HashSet<>();
+			for (Entry<MessageId, BdfDictionary> entry : metadata.entrySet()) {
+				BdfList authorList = entry.getValue().getList(KEY_AUTHOR);
+				Author a = clientHelper.parseAndValidateAuthor(authorList);
+				authors.add(a.getId());
+			}
+			// get information for all authors
+			Map<AuthorId, AuthorInfo> authorInfos = new HashMap<>();
+			for (AuthorId id : authors) {
+				authorInfos.put(id, authorManager.getAuthorInfo(txn, id));
+			}
+			// Parse the metadata
+			for (Entry<MessageId, BdfDictionary> entry : metadata.entrySet()) {
+				BdfDictionary meta = entry.getValue();
+				headers.add(getForumPostHeader(txn, entry.getKey(), meta,
+						authorInfos));
+			}
+			return headers;
 		} catch (FormatException e) {
 			throw new DbException(e);
 		}