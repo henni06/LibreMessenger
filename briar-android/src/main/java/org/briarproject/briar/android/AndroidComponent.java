briar/briar

Provide TransactionManager.

briar-android/src/main/java/org/briarproject/briar/android/AndroidComponent.java
@@ -14,6 +14,7 @@ import org.briarproject.bramble.api.contact.ContactManager;
 import org.briarproject.bramble.api.crypto.CryptoExecutor;
 import org.briarproject.bramble.api.crypto.PasswordStrengthEstimator;
 import org.briarproject.bramble.api.db.DatabaseExecutor;
+import org.briarproject.bramble.api.db.TransactionManager;
 import org.briarproject.bramble.api.event.EventBus;
 import org.briarproject.bramble.api.identity.IdentityManager;
 import org.briarproject.bramble.api.keyagreement.KeyAgreementTask;
@@ -168,6 +169,8 @@ public interface AndroidComponent
 	FeatureFlags featureFlags();
 
 	AndroidWakeLockManager wakeLockManager();
+
+	TransactionManager transactionManager();
 
 	void inject(SignInReminderReceiver briarService);
 

henni06/LibreMessenger

briar-android/src/main/java/org/briarproject/briar/android/AndroidComponent.java
@@ -46,6 +46,7 @@ import org.briarproject.briar.api.android.DozeWatchdog;
 import org.briarproject.briar.api.android.LockManager;
 import org.briarproject.briar.api.android.ScreenFilterMonitor;
 import org.briarproject.briar.api.attachment.AttachmentReader;
+import org.briarproject.briar.api.autodelete.AutoDeleteManager;
 import org.briarproject.briar.api.blog.BlogManager;
 import org.briarproject.briar.api.blog.BlogPostFactory;
 import org.briarproject.briar.api.blog.BlogSharingManager;
@@ -187,6 +188,8 @@ public interface AndroidComponent
 	CachingLogHandler logHandler();
 
 	Thread.UncaughtExceptionHandler exceptionHandler();
+
+	AutoDeleteManager autoDeleteManager();
 
 	void inject(SignInReminderReceiver briarService);
 