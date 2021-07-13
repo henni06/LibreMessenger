briar/briar

Add removable drive manager with placeholder task implementations.

bramble-core/src/main/java/org/briarproject/bramble/plugin/file/RemovableDrivePlugin.java
@@ -11,7 +11,7 @@ import java.io.OutputStream;
 
 import javax.annotation.concurrent.Immutable;
 
-import static org.briarproject.bramble.api.plugin.FileConstants.PROP_PATH;
+import static org.briarproject.bramble.api.plugin.file.FileConstants.PROP_PATH;
 import static org.briarproject.bramble.util.StringUtils.isNullOrEmpty;
 
 @Immutable

henni06/LibreMessenger

bramble-core/src/main/java/org/briarproject/bramble/plugin/file/RemovableDrivePlugin.java
@@ -11,7 +11,7 @@ import java.io.OutputStream;
 
 import javax.annotation.concurrent.Immutable;
 
-import static org.briarproject.bramble.api.plugin.RemovableDriveConstants.PROP_PATH;
+import static org.briarproject.bramble.api.plugin.file.RemovableDriveConstants.PROP_PATH;
 import static org.briarproject.bramble.util.StringUtils.isNullOrEmpty;
 
 @Immutable